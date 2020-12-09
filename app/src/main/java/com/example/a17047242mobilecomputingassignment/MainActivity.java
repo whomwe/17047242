b0fb2493d49679972e2d5d5fda4bf5dcbcdfe031
whomwe <63601983+whomwe@users.noreply.github.com>
2020-12-09 15:38:45 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a17047242mobilecomputingassignment/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.a17047242mobilecomputingassignment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.view.MotionEvent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView Apple1 = (ImageView) findViewById(R.id.Apple1);
+        Apple1.setOnTouchListener(handleTouch);
+    }
+
+    private View.OnTouchListener handleTouch = new View.OnTouchListener() {
+        float dX, dY;
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    dX = view.getX() - event.getRawX();
+                    dY = view.getY() - event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    view.animate()
+                            .x(event.getRawX() + dX)
+                            .y(event.getRawY() + dY)
+                            .setDuration(0)
+                            .start();
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+    };
+
+
+}
