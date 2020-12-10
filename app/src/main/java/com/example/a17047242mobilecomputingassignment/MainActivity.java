cbca76a5b98c9eef4bd8e0a7840b2c4421236662
whomwe <63601983+whomwe@users.noreply.github.com>
2020-12-10 04:17:27 +0000

    Drag apples

--- app/src/main/java/com/example/a17047242mobilecomputingassignment/MainActivity.java
+++ app/src/main/java/com/example/a17047242mobilecomputingassignment/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.view.MotionEvent;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,11 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView Apple1 = (ImageView) findViewById(R.id.Apple1);
         Apple1.setOnTouchListener(handleTouch);
+        ImageView Apple2 = (ImageView) findViewById(R.id.Apple2);
+        Apple2.setOnTouchListener(handleTouch);
+        ImageView Apple3 = (ImageView) findViewById(R.id.Apple3);
+        Apple3.setOnTouchListener(handleTouch);
+        ImageView Apple4 = (ImageView) findViewById(R.id.Apple4);
+        Apple4.setOnTouchListener(handleTouch);
+        ImageView Apple5 = (ImageView) findViewById(R.id.Apple5);
+        Apple5.setOnTouchListener(handleTouch);
+        ImageView Apple6 = (ImageView) findViewById(R.id.Apple6);
+        Apple6.setOnTouchListener(handleTouch);
+        ImageView Apple7 = (ImageView) findViewById(R.id.Apple7);
+        Apple7.setOnTouchListener(handleTouch);
+        ImageView Apple8 = (ImageView) findViewById(R.id.Apple8);
+        Apple8.setOnTouchListener(handleTouch);
+        ImageView Apple9 = (ImageView) findViewById(R.id.Apple9);
+        Apple9.setOnTouchListener(handleTouch);
+
     }
 
     private View.OnTouchListener handleTouch = new View.OnTouchListener() {
         float dX, dY;
-        int y;
         @Override
         public boolean onTouch(View view, MotionEvent event) {
             switch (event.getAction()) {
@@ -40,6 +59,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
-
+    
 
 }
